ed94fc9d8679152e45bf624c93743ba3e7df6899
Clipporion <marcusberger008@gmx.net>
2022-06-15 08:21:00 +0200

    Add function for early = and bugfixes
    I added a function to display the first entered number in case the =
    button is pressed before a second number has been entered.
    
    Also updated the function for calculations by pressing the operator
    button before pressing = so now it should display the correct result

--- javascript.js
+++ javascript.js
@@ -37,7 +37,7 @@ function operate(operator,a,b) {
         return divide(a,b)
         } else {
             firstLine.textContent = "Division by zero error";
-            secondLine.textContent = "Division by zero error";
+            return 0;
         }
     }
 }
@@ -59,19 +59,20 @@ numberButtons.forEach(button => {
 
 operatorButtons.forEach(button => {
     button.addEventListener("click", function(event) {
-        if (operator != "" && firstVariable[0] == final) {
+        if (operator != "" && secondVariable == "") {
             operator = "";
             operator += event.target.textContent;
             firstLine.textContent = `${firstVariable[0]}` + ` ` +event.target.textContent + ` `;
             secondLine.textContent = "";
-        } else if (operator != "" && firstVariable[0] != final) {
+        } else if (operator != "" && secondVariable != "") {
             firstVariable.push(+secondVariable);
             final = Math.floor(operate(operator,firstVariable[0],firstVariable[1])*1000)/1000;
-            firstLine.textContent += ` ` +event.target.textContent + ` `;
-            secondLine.textContent = `${final}`;
             firstVariable[0] = +final;
+            firstLine.textContent = `${firstVariable[0]}` + ` ` +event.target.textContent + ` `;
             firstVariable.splice(1);
             secondVariable = "";
+            operator = "";
+            operator += event.target.textContent;
         } else {
         firstVariable.push(+secondVariable);
         secondVariable = "";
@@ -82,11 +83,16 @@ operatorButtons.forEach(button => {
 
 resultButton.addEventListener("click", function() {
         firstVariable.push(+secondVariable);
+        if (firstVariable.length == 1 || !firstVariable[1]) {
+            secondLine.textContent = `${firstVariable[0]}`
+            firstVariable.splice(1);
+        } else {
         final = Math.floor(operate(operator,firstVariable[0],firstVariable[1])*1000)/1000;
         secondLine.textContent = `${final}`;
         firstVariable[0] = +final;
         firstVariable.splice(1);
         secondVariable = "";
+        }
     })
 
 clearButton.addEventListener("click", clear);
